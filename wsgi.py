912a92bcde7ebe7ee609fbb9099d58b72f9c7ece
Tu Shuo <294679339@qq.com>
2018-02-02 15:41:43 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python
+from flask import Flask
+application = Flask(__name__)
 
-print ("Content-type:text/html\r\n\r\n")
-print ('<html>')
-print ('<head>')
-print ('<title>Hello Word - First CGI Program</title>')
-print ('</head>')
-print ('<body>')
-print ('<h2>Hello Word! This is my first CGI program</h2>')
-print ('</body>')
-print ('</html>')
+@application.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    application.run()
