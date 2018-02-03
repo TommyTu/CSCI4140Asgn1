6dfaa1bcf4f6d37d44dacec1cb69d96337e265e3
Tu Shuo <294679339@qq.com>
2018-02-03 18:24:14 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ PORT = 8000
 #TODO: check that port is available,
 # and look for a different one if it isn't.
 
-script_path = "cgi-bin/hit-counter.py"
+script_path = "index.py"
 
 server_class = BaseHTTPServer.HTTPServer
 handler_class = CGIHTTPServer.CGIHTTPRequestHandler
