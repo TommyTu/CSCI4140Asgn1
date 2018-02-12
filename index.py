6a89e29c22a2aa7308c4232b738cd3c046303ac9
Tu Shuo <294679339@qq.com>
2018-02-12 18:43:49 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import cgi
+import cgitb
+import time
+import os
+cgitb.enable()
+
+print ("Content-Type: text/html")
+print ("""
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
+""")
