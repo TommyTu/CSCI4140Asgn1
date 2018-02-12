835d164a3b63eba4186ba2d10ec6773e44a6b338
Tu Shuo <294679339@qq.com>
2018-02-12 15:12:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import cgitb; cgitb.enable()  ## This line enables CGI error reporting
 if __name__ == "__main__":
     server = http.server.HTTPServer
     handler = http.server.CGIHTTPRequestHandler
-    ip = os.environ['OPENSHIFT_PYTHON_IP']
-    port = os.environ['OPENSHIFT_PYTHON_PORT']
-    host_name = os.environ['OPENSHIFT_GEAR_DNS']
-    server_address = (ip, port)
+  #  ip = os.environ['OPENSHIFT_PYTHON_IP']
+  #  port = os.environ['OPENSHIFT_PYTHON_PORT']
+  #  host_name = os.environ['OPENSHIFT_GEAR_DNS']
+   # server_address = (ip, port)
 
     httpd = server(("",8080), handler)
     httpd.serve_forever()
