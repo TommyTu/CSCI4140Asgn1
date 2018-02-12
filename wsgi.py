59a219899fcba938fe1eddcd1f27e51cdf68f63c
Tu Shuo <294679339@qq.com>
2018-02-12 14:27:59 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,13 @@
 import http.server,os
 import cgitb; cgitb.enable()  ## This line enables CGI error reporting
- 
-server = http.server.HTTPServer
-handler = http.server.CGIHTTPRequestHandler
-ip = os.environ['OPENSHIFT_PYTHON_IP']
-port = os.environ['OPENSHIFT_PYTHON_PORT']
-host_name = os.environ['OPENSHIFT_GEAR_DNS']
-server_address = (host_name, port)
- 
-httpd = server(server_address, handler)
-httpd.serve_forever()
+
+if __name__ == "__main__":
+    server = http.server.HTTPServer
+    handler = http.server.CGIHTTPRequestHandler
+    ip = os.environ['OPENSHIFT_PYTHON_IP']
+    port = os.environ['OPENSHIFT_PYTHON_PORT']
+    host_name = os.environ['OPENSHIFT_GEAR_DNS']
+    server_address = (host_name, port)
+
+    httpd = server(server_address, handler)
+    httpd.serve_forever()
