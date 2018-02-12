17a169cafe77a25a5a08499f055c038cb430fc01
Tu Shuo <294679339@qq.com>
2018-02-12 11:08:53 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,6 @@ ip = os.environ['OPENSHIFT_PYTHON_IP']
 port = os.environ['OPENSHIFT_PYTHON_PORT']
 host_name = os.environ['OPENSHIFT_GEAR_DNS']
 server_address = (host_name, port)
-handler.cgi_directories = ["cgi-bin/"]
  
 httpd = server(server_address, handler)
 httpd.serve_forever()
