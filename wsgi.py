032fdbe6e5edff0cccd917f4edbd82e6e815aeb5
Tu Shuo <294679339@qq.com>
2018-02-12 11:00:07 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,8 @@ server = http.server.HTTPServer
 handler = http.server.CGIHTTPRequestHandler
 ip = os.environ['OPENSHIFT_PYTHON_IP']
 port = os.environ['OPENSHIFT_PYTHON_PORT']
-server_address = (ip, port)
+host_name = os.environ['OPENSHIFT_GEAR_DNS']
+server_address = (host_name, port)
 handler.cgi_directories = ["cgi-bin/"]
  
 httpd = server(server_address, handler)
